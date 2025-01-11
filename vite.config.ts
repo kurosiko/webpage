8a1eb7a7aeaa1efac2b6cd94a9570acff4572217
kurosiko <kurosiko@outlook.com>
2025-01-11 12:17:54 +0900

    add dist directory

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig({  
   plugins: [svelte()],
 })
