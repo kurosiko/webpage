fef9aab47d7232623de51165a2eb79ee25e7c5f4
kurosiko <kurosiko@outlook.com>
2025-01-12 01:53:05 +0900

    add build files

---
+++ src/components/Logo.svelte
@@ -0,0 +1,3 @@
+<div class="m-5 rounded-lg border-white border-2 drop-shadow shadow-white text-center">
+    <h1 class="shadow-pink-400 [text-shadow:_3px_5px_0_var(--tw-shadow-color)]">Honor and revere the Ahoge.</h1>
+</div>
