2f8ffd3384f1cee56dff04d513d6a621aa79f7a2
kurosiko <kurosiko@outlook.com>
2025-03-23 02:44:09 +0900

    update bun.lockb

--- .svelte-kit/ambient.d.ts
+++ .svelte-kit/ambient.d.ts
@@ -49,6 +49,7 @@ declare module '$env/static/private' {
 	export const LOCALAPPDATA: string;
 	export const LOGONSERVER: string;
 	export const NODE: string;
+	export const NODE_ENV: string;
 	export const npm_command: string;
 	export const npm_config_local_prefix: string;
 	export const npm_config_user_agent: string;
@@ -157,6 +158,7 @@ declare module '$env/dynamic/private' {
 		LOCALAPPDATA: string;
 		LOGONSERVER: string;
 		NODE: string;
+		NODE_ENV: string;
 		npm_command: string;
 		npm_config_local_prefix: string;
 		npm_config_user_agent: string;
