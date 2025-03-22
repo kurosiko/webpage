87a9eb487037404e9370ee6a01d91b2b184e9f66
kurosiko <kurosiko@outlook.com>
2025-03-22 21:02:28 +0900

    diseable media/video

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,6 @@
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 import adapter from "@sveltejs/adapter-cloudflare"
+
 export default {
     preprocess: vitePreprocess(),
     kit: {
@@ -9,7 +10,6 @@ export default {
             precompress: false,
             fallback: "index.html",
             strict: true,
-            
         }),
     },
     compilerOptions: {
